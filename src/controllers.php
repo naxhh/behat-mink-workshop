eb749178d37fac9f508d8c60f6f163c48fcafa5a
Ignacio Tolstoy <arkantos798@gmail.com>
2014-06-28 17:30:16 -0400

    Js features. All working

--- src/controllers.php
+++ src/controllers.php
@@ -7,17 +7,17 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
-function userShouldBeLogin($app) {
-    if (! ($user = $app['session']->get('user')) ) {
-        return $app->redirect('/login');
-    }
-    return $user;
+function isLogged($app) {
+    return $app['session']->get('user');
 }
 
 $app->get('/', function () use ($app) {
-    $obj = new \Kitty\DataSource;
+    if (! ($user = isLogged($app)))
+    {
+        return $app->redirect('/login');
+    }
 
-    $user = array( 'username' => 'kitty69' );
+    $obj = new \Kitty\DataSource;
     $user = $obj->get( $user['username'] );
 
     $kitties = $obj->getList();
@@ -28,8 +28,45 @@ $app->get('/', function () use ($app) {
 ->bind('homepage')
 ;
 
+$app->get('/login', function() use ($app) {
+    if (isLogged($app))
+    {
+        return $app->redirect('/');
+    }
+
+    return $app['twig']->render('login.twig');
+})
+->bind('login')
+;
+
+$app->post('/login', function() use ($app) {
+    $username = $app['request']->request->get('username');
+    $password = $app['request']->request->get('password');
+
+    $obj = new \Kitty\DataSource;
+    $kitty = $obj->get($username);
+
+    if ($kitty && $password == $username) {
+        $app['session']->set('user', array('username' => $username ));
+        return $app->redirect('/');
+    }
+
+    return $app->redirect('/login');
+})
+;
+
+$app->get('/logout', function() use ($app) {
+    $app['session']->set('user', array());
+    return $app->redirect('/login');
+})
+;
+
 $app->get('/kitty/{name}', function ($name) use ($app) {
-    $user = array( 'username' => 'kitty69' );
+    if (! ($user = isLogged($app)))
+    {
+        return $app->redirect('/login');
+    }
+
     $obj = new \Kitty\DataSource;
     $kitty = $obj->get($name);
 
@@ -44,7 +81,10 @@ $app->get('/kitty/{name}', function ($name) use ($app) {
 ;
 
 $app->get('/kitty/{name}/society', function ($name) use ($app) {
-    $user = array( 'username' => 'kitty69' );
+    if (! ($user = isLogged($app)))
+    {
+        return $app->redirect('/login');
+    }
     $obj = new \Kitty\DataSource;
     $kitty = $obj->get($name);
 
@@ -59,56 +99,68 @@ $app->get('/kitty/{name}/society', function ($name) use ($app) {
 ;
 
 $app->get('/kitty/{name}/miau', function ($name) use($app) {
-    $user = array( 'username' => 'kitty69' );
+    if (! ($user = isLogged($app)))
+    {
+        return $app->json( array( 'msg' => 'You must be logged in' ), 403);
+    }
+
     $obj = new \Kitty\DataSource;
     $kitty = $obj->get($name);
 
     if (!$kitty)
     {
-        return $app->json( array( 'msg' => 'Woops! something went wrong' ), 404);
+        return $app->json( array( 'msg' => 'Woops! something went wrong', 'type' => 'warning' ), 404);
     }
 
     // using $name here can cause a bug.
     $kitty['interactions']['miau'][$user['username']] = time();
     $obj->set( $kitty );
 
-    return $app->json( array( 'msg' => 'You said hi! to ' . $name . ' luck in this adventure, maybe you want to catch some butterfly for him/her') );
+    return $app->json( array( 'msg' => 'You said hi! to ' . $name . ' luck in this adventure, maybe you want to catch some butterfly for him/her', 'type' => 'success') );
 })
 ->bind('miau')
 ;
 
 $app->get('/kitty/{name}/prrr', function($name) use($app) {
-    $user = array( 'username' => 'kitty69' );
+    if (! ($user = isLogged($app)))
+    {
+        return $app->json( array( 'msg' => 'You must be logged in', 'type' => 'warning' ), 403);
+    }
+
     $obj = new \Kitty\DataSource;
     $kitty = $obj->get($name);
 
     if (!$kitty)
     {
-        return $app->json( array( 'msg' => 'Woops! something went wrong' ), 404);
+        return $app->json( array( 'msg' => 'Woops! something went wrong', 'type' => 'warning' ), 404);
     }
 
     $kitty['interactions']['prrr'][$user['username']] = time();
     $obj->set( $kitty );
 
-    return $app->json( array( 'msg' => "You contacted with this pretty kitty! For sure you will have luck and win an extra life here, if you know what I mean....") );
+    return $app->json( array( 'msg' => "You contacted with this pretty kitty! For sure you will have luck and win an extra life here, if you know what I mean....", 'type' => 'success') );
 })
 ->bind('prrr')
 ;
 
 $app->get('/kitty/{name}/fzzzz', function($name) use($app) {
-    $user = array( 'username' => 'kitty69' );
+    if (! ($user = isLogged($app)))
+    {
+        return $app->json( array( 'msg' => 'You must be logged in', 'type' => 'warning' ), 403);
+    }
+
     $obj = new \Kitty\DataSource;
     $kitty = $obj->get($name);
 
     if (!$kitty)
     {
-        return $app->json( array( 'msg' => 'Woops! something went wrong' ), 404);
+        return $app->json( array( 'msg' => 'Woops! something went wrong', 'type' => 'warning' ), 404);
     }
 
     $kitty['interactions']['fzzzz'][$user['username']] = time();
     $obj->set( $kitty );
 
-    return $app->json( array( 'msg' => "oh! I can't belive we have a mice in our site! we will take care of him for sure. ") );
+    return $app->json( array( 'msg' => "oh! I can't belive we have a mice in our site! we will take care of him for sure. ", 'type' => 'success') );
 })
 ->bind('fzzzz') //setting here prrr makes prrr url not work :D
 ;
