f8d2ebdbb0be1f13864343cec8f3356cce83f1b7
Ignacio Tolstoy <arkantos798@gmail.com>
2014-07-01 14:02:26 -0400

    temp comment for cleanning the machine

---
+++ features/bootstrap/WebContext.php
@@ -0,0 +1,17 @@
+<?php
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+class WebContext extends MinkContext
+{
+    /**
+     * @Then /^I wait for the suggestion box to appear$/
+     */
+    public function iWaitForTheSuggestionBoxToAppear()
+    {
+        $this->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
+    }
+}
